0b3d911413728c31aff964c042549df3b5956af1
Paulo <pauloessau32@gmail.com>
2026-08-05 22:15:13 -0600

    Feat: Implementacion de productos en la pantalla de paquetes

--- src/app/ecommerce/features/paquete-hogar/paquete-hogar.ts
+++ src/app/ecommerce/features/paquete-hogar/paquete-hogar.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TopBarComponent } from '../../shared/components/topbar/topbar.component';
 import { CtaContactoComponent } from '../home/components/cta-contacto/cta-contacto';
+import { ProductoService } from '../../../gestion/features/productos/services/producto';
+import { Producto } from '../../../gestion/features/productos/productos';
 
 @Component({
   selector: 'app-paquete-hogar',
   standalone: true,
-  imports: [CommonModule, RouterModule, TopBarComponent, CtaContactoComponent],
+  imports: [CommonModule, RouterModule, TopBarComponent, CtaContactoComponent, CurrencyPipe],
   templateUrl: './paquete-hogar.html'
 })
-export class PaqueteHogarComponent {}
+export class PaqueteHogarComponent implements OnInit {
+  private productoService = inject(ProductoService);
+  productos = signal<Producto[]>([]);
+
+  ngOnInit() {
+    this.productoService.obtenerProductos().subscribe({
+      next: (data) => {
+        this.productos.set(data);
+      },
+      error: (err) => console.error('Error al obtener productos', err)
+    });
+  }
+}
